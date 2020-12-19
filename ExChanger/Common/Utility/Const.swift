86b584d3abd87f0176365e906077f6e2cf78c896
Shi Lei <shilei0090@gmail.com>
2020-12-19 12:50:52 +0900

    104 release

--- ExChanger/Common/Utility/Const.swift
+++ ExChanger/Common/Utility/Const.swift
@@ -10,23 +10,23 @@ import UIKit
 
 public struct Const {
     
-    #if RELEASE
+//    #if RELEASE
     public static let TOP_BANNER_AD_UNIT_ID = "ca-app-pub-1668811522927993/9961299741"
-    #else
-    public static let TOP_BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111"
-    #endif
+//    #else
+//    public static let TOP_BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111"
+//    #endif
     
-    #if RELEASE
+//    #if RELEASE
     public static let SECTION_BANNER_AD_UNIT_ID = "ca-app-pub-1668811522927993/8899832650"
-    #else
-    public static let SECTION_BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111"
-    #endif
+//    #else
+//    public static let SECTION_BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111"
+//    #endif
     
-    #if RELEASE
+//    #if RELEASE
     public static let INSTERSTITIAL_AD_UNIT_ID = "ca-app-pub-1668811522927993/4075068732"
-    #else
-    public static let INSTERSTITIAL_AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
-    #endif
+//    #else
+//    public static let INSTERSTITIAL_AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
+//    #endif
     
 
 }
